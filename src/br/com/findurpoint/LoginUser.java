6dbf8d4fc21aa3bd5b97188f76949ea873d8fcc3
Maria Damo <mdamodacosta@yahoo.com.br>
2022-09-12 20:48:13 -0300

    verificacao de senha adicionada

--- src/br/com/findurpoint/LoginUser.java
+++ src/br/com/findurpoint/LoginUser.java
@@ -1,9 +1,10 @@
 package br.com.findurpoint;
 
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class LoginUser extends AbstractEntity{
+
 
     private String senhaUser;
     private String confirmacaoSenha;
@@ -36,6 +37,38 @@ public class LoginUser extends AbstractEntity{
 
         this.home = home;
     }
+
+
+    public void verificaSenha() {
+        System.out.println("Senha: ");
+        Scanner entradaSenha = new Scanner(System.in);
+
+        LoginUser senhaUser = new LoginUser();
+        senhaUser.setSenhaUser(entradaSenha.next());
+
+        System.out.println("confirmação de senha: ");
+        Scanner entradaConfirmacaoSenha = new Scanner(System.in);
+
+        LoginUser confirmacaoSenha = new LoginUser();
+        confirmacaoSenha.setConfirmacaoSenha(entradaConfirmacaoSenha.next());
+
+//        System.out.println("senha é " + senhaUser.getSenhaUser());
+//        System.out.println("confirmação de senha é " + confirmacaoSenha.getConfirmacaoSenha());
+
+        if (senhaUser.getSenhaUser().equals(confirmacaoSenha.getConfirmacaoSenha())){
+            System.out.println("as senhas são iguais");
+
+        }else {
+            System.out.println("as senhas não são iguais");
+        }
+
+        entradaSenha.close();
+        entradaConfirmacaoSenha.close();
+
+
+
+    }
+
 
 
 }
